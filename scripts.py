93d9f23292225953dcdb4e29a4f7985b86934349
Ben Hung <kopnosema@gmail.com>
2021-01-17 13:21:28 -0600

    tickers

--- scripts.py
+++ scripts.py
@@ -1,3 +1,17 @@
 import praw
 import credentials
 
+file = open("tickers.txt", "r")
+words = file.read().splitlines()
+file.close()
+
+reddit = praw.Reddit(
+    client_id = credentials.client_id,
+    client_secret = credentials.client_secret,
+    user_agent = credentials.user_agent
+)
+
+subreddit = reddit.subreddit("investing")
+
+for submission in subreddit.hot(limit = 60):
+    print(submission.title)
